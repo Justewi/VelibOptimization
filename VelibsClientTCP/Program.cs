3bc5166919545146bc46aab52eb8839713e8df2f
Robin ALONZO <alonzo.robin7@gmail.com>
2017-05-21 20:59:10 +0200

    Modification des endpoints et création de 2 clients

--- VelibsClientTCP/Program.cs
+++ VelibsClientTCP/Program.cs
@@ -1,30 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using VelibsClient.VelibsService;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VelibsClientTCP.VelibsService;
 
-namespace VelibsClient
+namespace VelibsClientTCP
 {
     class Program
     {
         static void Main(string[] args)
         {
-            VelibsOptimisationClient client = new VelibsOptimisationClient("SOAP");
+            VelibsOptimisationClient client = new VelibsOptimisationClient("TCP");
 
             Console.Write("Starting point address: ");
             string start = Console.ReadLine();
             Console.Write("Ending point address: ");
             string end = Console.ReadLine();
-            
+
             Console.WriteLine();
-            
+
             string[] response = client.GetDirections(start, end);
-            foreach(string str in response){
+            foreach (string str in response)
+            {
                 Console.WriteLine(str);
             }
-            
+
             Console.WriteLine();
             Console.WriteLine("Press Enter to quit");
-            
+
             Console.ReadLine();
         }
     }
